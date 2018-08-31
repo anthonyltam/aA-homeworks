ba99c08bba6391381195a0b9ae3947565ecaeacb
anthony <anthonyltam@gmail.com>
2018-08-31 07:35:20 -0700

    finish lru cache

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,18 +1,33 @@
 class LRUCache
-  
-  def initialize
+
+  attr_accessor :storage
+
+  def initialize(n)
+    @size = n
+    @storage = []
   end
 
   def count
     # returns number of elements currently in cache
+    @storage.length
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    if @storage.include?(el)
+      @storage.delete(el)
+      @storage << el
+    elsif count == @size
+      @storage.shift
+      @storage << el
+    else
+      @storage << el
+    end
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    p @storage
   end
 
   private
