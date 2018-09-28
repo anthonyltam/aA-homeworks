713dd503b567b3b2865d9c92d5f99f9c6bd5ef59
anthony <anthonyltam@gmail.com>
2018-09-27 23:20:31 -0700

    fail to setup config

---
+++ skeleton/webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./index.jsx",
+  output: {
+      path: path.resolve(__dirname),
+      filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+
+};
